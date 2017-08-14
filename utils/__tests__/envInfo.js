0249fd3fd82f72b81200cb4b92b0c5822fc4a26e
george <george@macbook-2.local>
2017-08-13 18:46:33 -0600

    update to handle _dirname, update tests

--- utils/__tests__/envInfo.js
+++ utils/__tests__/envInfo.js
@@ -2,9 +2,16 @@ const repo = 'g3org3-generator'
 
 describe('envInfo', () => {
   it('check env info', () => {
-    const envInfo = require('../envInfo')
+    const envInfo = require('../envInfo')()
     expect(envInfo.SCRIPTS_DIR.split('/').pop()).toBe(repo)
     expect(envInfo.CURRENT_DIR_NAME).toBe(repo)
     expect(envInfo.SCRIPTS_DIR).toBe(envInfo.CURRENT_DIR)
+  })
+
+  it('check env info', () => {
+    const envInfo = require('../envInfo')(__dirname)
+    expect(envInfo.SCRIPTS_DIR.split('/').pop()).toBe('__tests__')
+    expect(envInfo.CURRENT_DIR_NAME).toBe(repo)
+    expect(envInfo.SCRIPTS_DIR).toBe(__dirname)
   })
 })
--- utils/envInfo.js
+++ utils/envInfo.js
@@ -1,6 +1,6 @@
 
-module.exports = ({
-  SCRIPTS_DIR: require('path').resolve(__dirname, '../'),
+module.exports = (_dirname) => ({
+  SCRIPTS_DIR: _dirname || require('path').resolve(__dirname, '../'),
   CURRENT_DIR: process.env.PWD,
   CURRENT_DIR_NAME: process.env.PWD.split('/').pop(),
 })
