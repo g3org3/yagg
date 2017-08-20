48d55bb7e8ceb8007a5d105643c1f2960f11e0eb
george <george@macbook-2.local>
2017-08-20 13:17:00 -0600

    add to pkg script

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 npm t
+if [[ `node_modules/.bin/prettier-eslint --list-different "lib/**/*.js"` ]]; then
+  exit 0;
+else
+  echo "Please run npm run format"
+  exit 1;
+fi
--- pre-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [[ `node_modules/.bin/prettier-eslint --list-different "lib/**/*.js"` ]]; then
-  exit 0;
-else
-  echo "Formatting lib/**/*.js, please commit these files"
-  npm run format
-  git status
-fi
